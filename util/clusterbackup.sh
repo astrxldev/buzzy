24b63fe47a06744f7b6a93bfce1c7aff47029b8c
Dreamgineer <superdinmc@gmail.com>
2025-10-18 09:20:16 -0400

    Backup command

---
+++ util/clusterbackup.sh
@@ -0,0 +1,8 @@
+sudo echo "Root obtained."
+echo Backing up
+cd ~superdinmc/kpspnextgen
+docker compose exec -it db bash -c "pg_dumpall -h localhost -p 5432 -U postgres > /pgdata/cluster.sql"
+echo -n "Copying backup file to "
+cd -
+sudo cp /var/lib/docker/volumes/kpspnextgen_pgdata/_data/cluster.sql ./cluster.sql
+sudo chown superdinmc:users ./cluster.sql
