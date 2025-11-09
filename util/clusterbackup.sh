b34c4bcead32c9c38345553b9b257c66fca6151e
Dreamgineer <superdinmc@gmail.com>
2025-11-09 00:08:48 -0500

    update cluster backup script

--- util/clusterbackup.sh
+++ util/clusterbackup.sh
@@ -1,8 +1,8 @@
 sudo echo "Root obtained."
 echo Backing up
-cd ~superdinmc/kpspnextgen
+cd ~dmgnr/kpspnextgen
 docker compose exec -it db bash -c "pg_dumpall -h localhost -p 5432 -U postgres > /pgdata/cluster.sql"
 echo -n "Copying backup file to "
 cd -
 sudo cp /var/lib/docker/volumes/kpspnextgen_pgdata/_data/cluster.sql ./cluster.sql
-sudo chown superdinmc:users ./cluster.sql
+sudo chown $USER:users ./cluster.sql
