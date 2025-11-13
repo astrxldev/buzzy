7ca308b6d13506304d33776d0367ee0e666e3a4f
superdinmc <superdinmc@gmail.com>
2025-11-13 15:02:37 +0700

    fix wrong count

--- app/api/active/route.ts
+++ app/api/active/route.ts
@@ -7,13 +7,15 @@ export function GET({ signal }: Request) {
     topic: "active",
     event: "version",
   });
-  sse.publish(sse.count("active"), {
+  // +1 because this connection is not yet counted
+  sse.publish(sse.count("active") + 1, {
     topic: "active",
     event: "count",
   });
 
   return sse.new("active", {
     onDisconnect() {
+      // no need to -1 because this connection is already removed
       sse.publish(sse.count("active"), { topic: "active", event: "count" });
     },
     signal,
