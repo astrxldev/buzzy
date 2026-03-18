7d29a5d5ddc8f56b81f8246800934a8367ed66a2
dmgnr <me@dgnr.us>
2026-03-19 01:36:45 +0700

    feat(guide): sprinkle some seo

--- app/(ui)/guide/page.tsx
+++ app/(ui)/guide/page.tsx
@@ -1,5 +1,11 @@
+import type { Metadata } from "next";
 import { searchGuide } from "./api";
 import { GuideList } from "./client";
+
+export const metadata: Metadata = {
+  title: "ไกด์ตัวละคร",
+  description: "ไกด์ปั้นตัวละครโดยเกนชินไม่ใช่เกมมือถือ",
+};
 
 export default async function GuidePage() {
   const initialData = await searchGuide("");
