4402cf3dedf300edf3c752978759d3392954c909
dmgnr <me@dgnr.us>
2026-06-10 16:39:49 +0700

    style: white border widget

--- app/widget/donate/top/page.tsx
+++ app/widget/donate/top/page.tsx
@@ -47,7 +47,7 @@ export default function TopDonateWidget() {
     return () => clearInterval(interval);
   }, [textRef, top]);
   return top ? (
-    <div className="grid h-18.75 w-94.5 max-w-94.5 grid-cols-[40px_minmax(0,1fr)_max-content] gap-1 rounded-full border-4 border-white bg-black/75 p-2 px-3 text-5xl font-semibold text-[#FFBA00] *:self-center">
+    <div className="grid h-18.75 w-94.5 max-w-94.5 grid-cols-[40px_minmax(0,1fr)_max-content] gap-1 rounded-full border-4 border-black bg-black/75 p-2 px-3 text-5xl font-semibold text-[#FFBA00] *:self-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         height="24px"
