c39b7a7a3b4bb42fe81d7e75a1df898ddd4dc387
dmgnr <me@dgnr.us>
2026-07-13 01:00:52 +0700

    fix(posthog): use organizational proxy

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,7 +2,7 @@ import posthog from "posthog-js";
 
 if (process.env.NODE_ENV === "production")
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
-    api_host: "/i",
+    api_host: "https://s.astrxl.dev",
     ui_host: "https://us.posthog.com",
     defaults: "2026-01-30",
     capture_exceptions: true,
