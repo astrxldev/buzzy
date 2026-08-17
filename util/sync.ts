5f6e18e297ad7a56ead08b0f24946dc7bed424c5
gunshiz <spsshop1047@gmail.com>
2026-08-17 20:58:22 +0700

    fix : correct icon

--- util/sync.ts
+++ util/sync.ts
@@ -100,7 +100,7 @@ const extra: Avatar[] = [
     region: "MAINACTOR",
     specialProp: "FIGHT_PROP_ATTACK_PERCENT",
     bodyType: "BOY",
-    icon: "custom/traveler_dendro.webp:image/webp",
+    icon: "custom/traveler_cyro.webp:image/webp",
     birthday: [0, 0],
     route: "Cyro Traveler Boy",
     release: 1657659700,
