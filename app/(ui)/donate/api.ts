507e715642d59bab01cb3aaf8d5590ca2a3a72a3
dmgnr <me@dgnr.us>
2026-08-28 13:19:11 +0700

    chore: change to secret-based real promptpay identifier

--- app/(ui)/donate/api.ts
+++ app/(ui)/donate/api.ts
@@ -1,10 +1,18 @@
 "use server";
 
 import { generatePromptPayPayload } from "@/lib/promptpay";
+import { env } from "bun";
+
+const { PROMPTPAY_IDENTIFIER, PROMPTPAY_TYPE } = env;
+if (!PROMPTPAY_IDENTIFIER || !PROMPTPAY_TYPE)
+  console.error("Warning: Missing PromptPay configuration!");
 
 export async function generateQrcode(amount: number | string) {
   return generatePromptPayPayload({
-    identifier: { type: "nationalId", value: "1729900808055" },
+    identifier: {
+      type: PROMPTPAY_TYPE! as "mobile" | "nationalId" | "ewallet",
+      value: PROMPTPAY_IDENTIFIER!,
+    },
     amount,
   });
 }
